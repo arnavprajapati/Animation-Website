a416622cf77626e54d5e02b2db68577cc9074ea6
Arnav <arnavprajapati3101@gmail.com>
2025-04-12 16:39:17 +0530

    page1 done

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function loadingAnimation() {
         opacity:0
     })
 
-    tl.from(".hero h1",{
+    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
         y:120,
         stagger:0.2
     })
